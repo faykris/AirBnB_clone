298b3b22cae0576754c2f619c606008aa8e2e100
faykris <faykris28@gmail.com>
2021-11-08 22:44:11 -0500

    [update] change test user cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import unittest
+from datetime import datetime
+
 from models.user import User
 from models.base_model import BaseModel
 
@@ -21,22 +23,32 @@ class TestUser(unittest.TestCase):
     def test_email(self):
         """test email"""
         self.assertTrue(self.User.email)
+        self.assertIs(type(self.User.email), str)
         self.assertEqual(self.User.email, "2617@holbertonschool.com")
 
     def test_password(self):
         """test password"""
         self.assertTrue(self.User.password)
+        self.assertIs(type(self.User.password), str)
         self.assertEqual(self.User.password, "root")
 
     def test_first_name(self):
         """test first name"""
         self.assertTrue(self.User.first_name)
+        self.assertIs(type(self.User.first_name), str)
         self.assertEqual(self.User.first_name, "Cristian")
 
     def test_last_name(self):
         """test last name"""
         self.assertTrue(self.User.last_name)
+        self.assertIs(type(self.User.last_name), str)
         self.assertEqual(self.User.last_name, "Pinzon")
+
+    def test_attr_types(self):
+        """test id, created and update types"""
+        self.assertIs(type(self.User.id), str)
+        self.assertIs(type(self.User.created_at), datetime)
+        self.assertIs(type(self.User.updated_at), datetime)
 
 
 if __name__ == "__main__":
