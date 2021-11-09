7c2a1d900a560343d2acc56aab503355d53d63b7
faykris <faykris28@gmail.com>
2021-11-08 22:27:59 -0500

    [update] change test user cases

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,37 +8,36 @@ from models.base_model import BaseModel
 class TestUser(unittest.TestCase):
     """ Test """
 
+    User = User()
+    User.email = "2617@holbertonschool.com"
+    User.password = "root"
+    User.first_name = "Cristian"
+    User.last_name = "Pinzon"
+
     def test_User(self):
         """test_user"""
         self.assertTrue(True)
 
     def test_email(self):
         """test email"""
-        obj = User()
-        obj.email = "holbie@holbertonschool.com"
-        self.assertTrue(obj.email)
-        self.assertEqual(obj.email, "holbie@holbertonschool.com")
+        self.assertTrue(self.User.email)
+        self.assertEqual(self.User.email, "2617@holbertonschool.com")
 
     def test_password(self):
         """test password"""
-        obj = User()
-        obj.password = "root"
-        self.assertTrue(obj.password)
-        self.assertEqual(obj.password, "root")
+        self.assertTrue(self.User.password)
+        self.assertEqual(self.User.password, "root")
 
     def test_first_name(self):
         """test first name"""
-        obj = User()
-        obj.first_name = "Julien"
-        self.assertTrue(obj.first_name)
-        self.assertEqual(obj.first_name, "Julien")
+        self.assertTrue(self.User.first_name)
+        self.assertEqual(self.User.first_name, "Cristian")
 
     def test_last_name(self):
         """test last name"""
-        obj = User()
-        obj.last_name = "Barbier"
-        self.assertTrue(obj.last_name)
-        self.assertEqual(obj.last_name, "Barbier")
+        self.assertTrue(self.User.last_name)
+        self.assertEqual(self.User.last_name, "Pinzon")
+
 
 if __name__ == "__main__":
     unittest.main()
